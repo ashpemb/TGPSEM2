f645e1a618aab073688eff8bcb7b50e5cce2da74
unknown <t018471e@student.staffs.ac.uk>
2016-02-08 02:49:03 +0000

    t018471e - added platforms file to solution, added create method and touch events to platforms

--- Classes/Platforms.cpp
+++ Classes/Platforms.cpp
@@ -2,12 +2,56 @@
 
 USING_NS_CC;
 
-Platforms::Platforms()
+using namespace cocostudio::timeline;
+
+bool Platforms::init()
+{
+
+	srand(time(NULL));
+
+
+	auto rootNode = CSLoader::createNode("MainScene.csb");
+
+	cocos2d::Size frameSize = cocos2d::Director::getInstance()->getOpenGLView()->getFrameSize();
+
+	//Set up a touch listener.
+	auto touchListener = EventListenerTouchOneByOne::create();
+
+	//Set callbacks for our touch functions.
+	touchListener->onTouchBegan = CC_CALLBACK_2(Platforms::onTouchBegan, this);
+	touchListener->onTouchEnded = CC_CALLBACK_2(Platforms::onTouchEnded, this);
+	touchListener->onTouchMoved = CC_CALLBACK_2(Platforms::onTouchMoved, this);
+	touchListener->onTouchCancelled = CC_CALLBACK_2(Platforms::onTouchCancelled, this);
+
+	//Add our touch listener to event listener list.
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener, this);
+
+	return true;
+}
+
+void Platforms::CreateSprite(std::string tex, int x, int y)
+{
+	sprite = Sprite::create(tex);
+	sprite->setPosition(Vec2(x, y));
+}
+
+//Touch Functions
+void Platforms::onTouchBegan(Touch* touch, Event* event)
 {
 
 }
 
-Platforms::~Platforms()
+void Platforms::onTouchEnded(Touch* touch, Event* event)
 {
+	
+}
 
+void Platforms::onTouchMoved(Touch* touch, Event* event)
+{
+	
+}
+
+void Platforms::onTouchCancelled(Touch* touch, Event* event)
+{
+	
 }
--- Classes/Platforms.h
+++ Classes/Platforms.h
@@ -1,15 +1,32 @@
-#pragma once
+#ifndef __Platforms_SCENE_H__
+#define __Platforms_SCENE_H__
+
 #include "cocos2d.h"
+#include "cocostudio/CocoStudio.h"
 #include "ui/CocosGUI.h"
+#include "Define.h"
 
 class Platforms
 {
 public:
 
-	Platforms();
-	~Platforms();
+	virtual bool init();
+	void CreateSprite(std::string tex, int x, int y);
+
+
+
+	cocos2d::Sprite* sprite;
+
+	// Touch input
+	virtual void onTouchBegan(cocos2d::Touch*, cocos2d::Event*);
+	virtual void onTouchEnded(cocos2d::Touch*, cocos2d::Event*);
+	virtual void onTouchMoved(cocos2d::Touch*, cocos2d::Event*);
+	virtual void onTouchCancelled(cocos2d::Touch*, cocos2d::Event*);
+
 
 
 private:
 	
 };
+
+#endif // __Platforms_SCENE_H__
