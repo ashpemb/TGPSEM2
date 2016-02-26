5fac388c0806b6f98cfa3c4ad77459396c046ebe
s012721e <david@penkridge.co.uk>
2016-02-26 14:37:12 +0000

    S012721e - Added a splash screen with the Hopping Vikings logo and pixel images of all the Viking members

--- Classes/SplashScreenScene.h
+++ Classes/SplashScreenScene.h
@@ -2,23 +2,44 @@
 #define __SPLASHSCREEN_SCENE_H__
 
 #include "cocos2d.h"
+#include "cocostudio/CocoStudio.h"
 
-class SplashScreenScene : public cocos2d::Layer
+class SplashScreenScene : public cocos2d::LayerColor
 {
+private:
+	void TimeMainMenu(float time);
+
+	//cocos2d::Sprite*		_background;
+
+	// Timers
+	bool	_complete;
+	float	_vikingTime;
+	float	_davidTime;
+	float	_ashTime;
+	float	_alexTime;
+	float	_joshTime;
+	float	_jamesTime;
+	float	_mattTime;
+	float	_holdTime;
+
+	// Sprites
+	cocos2d::Sprite* _vikingLogo;
+	cocos2d::Sprite* _david;
+	cocos2d::Sprite* _ash;
+	cocos2d::Sprite* _alex;
+	cocos2d::Sprite* _josh;
+	cocos2d::Sprite* _james;
+	cocos2d::Sprite* _matt;
 public:
 	// there's no 'id' in cpp, so we recommend returning the class instance pointer
 	static cocos2d::Scene* createScene();
 
 	// Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
 	virtual bool init();
+	void update(float delta);
 
 	// implement the "static create()" method manually
 	CREATE_FUNC(SplashScreenScene);
-
-private:
-	void TimeMainMenu(float time);
-
-	cocos2d::Sprite*		_background;
 
 };
 
